76b3c2ce86fa5836b548faba8294c8b1d1ae59b7
Andre-Jacques de Beer <a.j.debeer@gmail.com>
2020-05-12 13:19:58 -0400

    can request visits with two viable sets of params. fuzzy matching not yet implemented

--- controllers/visit.js
+++ controllers/visit.js
@@ -3,6 +3,32 @@ const controller = {};
 
 controller.fetch = async (req, res) => {
   console.log("get visit endpoint hit");
+  try {
+    const { userId, visitId, searchString } = req.query;
+    let visits;
+    if (typeof visitId !== 'undefined') {
+      visits = await models.visits.findAll({ where: { id: visitId } });
+    }
+    else if (typeof userId !== 'undefined' && typeof searchString !== 'undefined') {
+      const lastFiveVisits = await models.visits.findAll({
+        where: { userId },
+        order: [['createdAt', 'DESC']],
+        limit: 5
+      });
+      visits = lastFiveVisits;
+    }
+    else {
+      res.status(422);
+      return res.send(`requests to this endpoint require either a 'visitId' or both a 'userId' and a 'searchString' property`);
+    }
+    res.status(200);
+    return res.json(visits);
+  }
+  catch(err) {
+    console.log("error fetching visits", err);
+    res.status(500);
+    return res.send('an unknown error occurred')
+  }
 };
 
 controller.create = async (req, res) => {
