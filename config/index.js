0df8c5e29bb460553fa8bc3f96397df186e17bf5
Andre-Jacques de Beer <a.j.debeer@gmail.com>
2020-05-12 13:03:38 -0400

    updated env variable name to match heroku

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
-console.log('connecting to database', process.env.DATABASE);
+console.log('connecting to database', process.env.DATABASE_URL);
 
 module.exports = {
-  db: process.env.DATABASE
+  db: process.env.DATABASE_URL
 };
